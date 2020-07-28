f05e83a9b97915b1fef913ae0941e43b90f6fa81
Akramul-Hoque <akramulhoque105@gmail.com>
2020-07-28 23:45:50 +0600

    2nd commit

---
+++ PHP_SESSION_TASK_04/regCheck.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+
+	if(isset($_POST['submit'])){
+
+		$uname = $_POST['username'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		$enc = md5($password);
+
+		if(empty($uname) || empty($password) || empty($email)){
+			echo "null submission";
+		}else {
+
+			$user = [
+						'uname'=>$uname,
+						'email'=>$email,
+						'password'=>$password
+					];
+
+			setcookie('uname', $uname, time()+3600, '/');
+			setcookie('email', $email, time()+3600, '/');
+			setcookie('password', $enc, time()+3600, '/');
+
+			header('location: login.html');
+		}
+
+	}else{
+		header("location: login.html");
+	}
+
+
+?> 
