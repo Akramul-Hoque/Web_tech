81b65ef7e601839270d9bdf65afd21d949419e72
Akramul-Hoque <akramulhoque105@gmail.com>
2020-08-25 09:16:57 +0600

    email

--- FINAL_LABTASK_3/FORM.js
+++ FINAL_LABTASK_3/FORM.js
@@ -5,6 +5,8 @@
 function validate(){
 
 	var username = document.getElementById('username').value;
+	var email = document.getElementById('email').value;
+
 	
 	
 	
@@ -13,20 +15,35 @@ function validate(){
 		document.getElementById('userMsg').innerHTML = "username can't left empty";
 		return false;
 
-	}else{
+	}
+	 var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!filter.test(email.value)) {
+    alert('Please provide a valid email address');
+    email.focus;
+    return false;
+	}
+	else{
 		return true;
 	}
 
 
+
 }
+
+
+
 
 function remover(){
 	document.getElementById('userMsg').innerHTML = "";
+	document.getElementById('emailMsg').innerHTML = "";
 }
 
 function xyz(){
 
 	if(document.getElementById('username').value == ""){
 		document.getElementById('userMsg').innerHTML = "this field is required!";
+	}if(document.getElementById('email').value == ""){
+		document.getElementById('emailMsg').innerHTML = "this field is required!";
 	}
 }
